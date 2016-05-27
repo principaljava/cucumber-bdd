e241fe333b9be276fab8f8be926d279a2f191f97
ravia <ravia@giffgaffMravia.local>
2016-05-27 12:29:09 +0100

    initial commit

---
+++ src/main/java/com/cucumber/bdd/thinkcode/itake/Belly.java
@@ -0,0 +1,21 @@
+package com.cucumber.bdd.thinkcode.itake;
+
+/**
+ * Hello world!
+ *
+ */
+public class Belly {
+    private int cukes;
+
+    public void eat(int cukes) {
+        this.cukes = cukes;
+    }
+
+    public String getSound(int waitingTime) {
+        if (cukes > 41 && waitingTime >= 1) {
+            return "growl";
+        } else {
+            return "silent";
+        }
+    }
+}
---
+++ src/test/java/com/cucumber/bdd/thinkcode/itake/RunCukesTest.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.cucumber.bdd.thinkcode.itake;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+
+/**
+ * @author ravia
+ *
+ */
+@RunWith(Cucumber.class)
+public class RunCukesTest {
+
+}
